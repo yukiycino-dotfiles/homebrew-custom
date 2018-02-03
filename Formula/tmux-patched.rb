44fe832f63f585c17727d7434b33be8c8f4ebf99
yuki_yano <yuki.ycino@gmail.com>
2018-02-03 18:32:29 +0900

    first commit

---
+++ Formula/tmux-patched.rb
@@ -0,0 +1,66 @@
+class TmuxPatched < Formula
+  desc "Terminal multiplexer"
+  homepage "https://tmux.github.io/"
+  url "https://github.com/tmux/tmux/releases/download/2.6/tmux-2.6.tar.gz"
+  sha256 "b17cd170a94d7b58c0698752e1f4f263ab6dc47425230df7e53a6435cc7cd7e8"
+
+  bottle do
+    sha256 "0ca2e76822980dcc009fde38379f7546b6568975d9dc4f2a6a312e32fce186f8" => :high_sierra
+    sha256 "d2b71640c44c4fc1e953a6eb1ca14b8c91ee4a19a91f2f699546c5cd6ed5b302" => :sierra
+    sha256 "cda9003fa113251c024210750d529be80379288436cf66a67a3896d2d14b6766" => :el_capitan
+  end
+
+  head do
+    url "https://github.com/tmux/tmux.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "libevent"
+  depends_on "utf8proc" => :optional
+
+  resource "completion" do
+    url "https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/homebrew_1.0.0/completions/tmux"
+    sha256 "05e79fc1ecb27637dc9d6a52c315b8f207cf010cdcee9928805525076c9020ae"
+  end
+
+  def patches
+    [
+      'https://gist.githubusercontent.com/waltarix/1399751/raw/4db15139bcbf2d36993ef700b43a69eb0392aaf2/tmux-do-not-combine-utf8.patch',
+      'https://gist.githubusercontent.com/waltarix/1399751/raw/4db15139bcbf2d36993ef700b43a69eb0392aaf2/tmux-pane-border-ascii.patch'
+    ]
+  end
+
+  def install
+    system "sh", "autogen.sh" if build.head?
+
+    args = %W[
+      --disable-Dependency-tracking
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+    ]
+
+    args << "--enable-utf8proc" if build.with?("utf8proc")
+
+    ENV.append "LDFLAGS", "-lresolv"
+    system "./configure", *args
+
+    system "make", "install"
+
+    pkgshare.install "example_tmux.conf"
+    bash_completion.install resource("completion")
+  end
+
+  def caveats; <<~EOS
+    Example configuration has been installed to:
+      #{opt_pkgshare}
+    EOS
+  end
+
+  test do
+    system "#{bin}/tmux", "-V"
+  end
+end
---
+++ README.md
@@ -0,0 +1,18 @@
+# homebrew-custom
+
+ Keep a modified formula for myself.
+
+## Packages
+
+-   tmux-patched
+
+## Install
+
+```text
+$ brew install yukiycino-dotfiles/custom/tmux-patched
+```
+
+## Thanks
+
+-   Fix a problems with displaying Ambiguous-width
+    -   <https://gist.github.com/waltarix/1399751>
